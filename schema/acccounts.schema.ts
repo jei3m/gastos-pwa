2fb7776d5b942fb8ef2b80293720de8b6dd3da96
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-14 00:57:39 +0800

    feat: (#9) update account schema

--- schema/acccounts.schema.ts
+++ schema/acccounts.schema.ts
@@ -9,3 +9,13 @@ export const createAccountSchema = z.object({
     }),
     description: z.string(),
 });
+
+export const updateAccountSchema = z.object({
+    name: z.string().max(10, {
+        message: "Name has a maximum of 10 characters only",
+    }),
+    type: z.string().min(1,{
+        message: "Please select an account type",
+    }),
+    description: z.string(),
+});
