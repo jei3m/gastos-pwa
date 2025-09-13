e7f84c88fb57e1dd1a23f183b1904148ab83e4f6
Justin Miguel Reyes <virozzegt@gmail.com>
2025-09-13 15:32:05 +0800

    feat: (#8) GET, CREATE accounts API

--- app/api/accounts/route.ts
+++ app/api/accounts/route.ts
@@ -1,66 +1,81 @@
 import { NextRequest } from 'next/server';
 import crypto from 'crypto'
 import { db } from '@/utils/db';
-import { 
-  createAccounts, 
-  getAccounts
+import {
+	createAccounts,
+	getAccounts
 } from '@/sql/accounts/accounts.sql';
-import { 
-  success, 
-  fail 
+import {
+	success,
+	fail
 } from '@/utils/helpers';
 import { responseRow } from '@/types/response.types';
 import { fetchUserID } from '@/utils/session';
 
 // Create New Account
 export async function POST(req: NextRequest) {
-  try {
-    const { 
-      name, 
-      type, 
-      description 
-    } = await req.json();
-    
-    const [resultCreate] = await db.query<responseRow[]>(
-      createAccounts(), 
-      { 
-        actionType: 'create',
-        uuid: crypto.randomUUID(),
-        userID: await fetchUserID(),
-        name,
-        type,
-        description 
-      }
-    );
+	try {
+		const {
+			name,
+			type,
+			description
+		} = await req.json();
 
-    return success({ 
-      response: JSON.parse(resultCreate[1][0].response)
-    });
+		const [resultCreate] = await db.query<responseRow[]>(
+			createAccounts(),
+			{
+				actionType: 'create',
+				uuid: crypto.randomUUID(),
+				userID: await fetchUserID(),
+				name,
+				type,
+				description
+			}
+		);
 
-  } catch (error) {
-    return fail(
-      error instanceof Error 
-        ? error.message 
-        : 'Failed to Create Tests'
-    );
-  }
+		const parsedData = JSON.parse(
+			resultCreate[1][0].response
+		);
+
+		if (parsedData.responseCode !== 200) {
+			return fail(
+				parsedData.responseCode,
+				parsedData.responseMessage
+			)
+		};
+
+		return success({
+			data: parsedData
+		});
+
+	} catch (error) {
+		return fail(
+			500,
+			error instanceof Error
+				? error.message
+				: 'Failed to Create Tests'
+		);
+	}
 };
 
 // Get All Accounts of the user
 export async function GET() {
-  try {
-    const [rows] = await db.query(
-      getAccounts(),
-      {
-        userID: await fetchUserID()
-      }
-    );
+	try {
+		const [rows] = await db.query(
+			getAccounts(),
+			{
+				userID: await fetchUserID()
+			}
+		);
 
-    return success({data: rows});
-    
-  } catch (err) {
-    return fail(
-      err instanceof Error ? err.message : "Failed to Fetch Accounts"
-    );
-  }
+		return success({ data: rows });
+
+	} catch (error) {
+		return fail(
+			500,
+			error instanceof Error 
+				? error.message 
+				: "Failed to Fetch Accounts"
+		);
+	}
 };
