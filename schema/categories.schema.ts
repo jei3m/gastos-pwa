09a6c64e35bb651bbfaf621bf05e7a131a4e8107
Justin Miguel Reyes <virozzegt@gmail.com>
2025-10-05 21:15:45 +0800

    feat: (#7) categories schema

---
+++ schema/categories.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const createCategorySchema = z.object({
+    name: z.string().max(15, {
+        message: 'Name has a maximum of 15 characters only',
+    }),
+    type: z.string().min(1, {
+        message: 'Please select a category type',
+    })
+});
