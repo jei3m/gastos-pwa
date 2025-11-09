83b1383d3bf39531dd9d715c5efde22e28275e47
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-09 20:12:18 +0800

    feat: (#6) createTransaction store function

---
+++ store/transactions.store.ts
@@ -0,0 +1,22 @@
+import { 
+  CreateTransaction
+} from "@/types/transactions.types";
+
+
+export const createTransaction = async(transaction: CreateTransaction) => {
+  try {
+    const res = await fetch('/api/transactions', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(transaction)
+    });
+    const data = await res.json();
+    if(!res.ok) throw Error(data.message);
+    return(data.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error(error.message);
+    };
+    throw Error('Failed to Create Transaction');
+  }
+};
