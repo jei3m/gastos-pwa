4438626aa2156aa13ba38f5bc547a43b90e651d0
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-16 19:08:02 +0800

    feat: (#6) transaction interface

--- types/transactions.types.ts
+++ types/transactions.types.ts
@@ -1,11 +1,18 @@
 export interface Transaction {
-  uuid: string,
-  amount: number,
-  type: string,
-  time: string,
   date: string,
-  categoryID: string
+  total: string,
+  details: TransactionDetails[],
+  userID: string
 };
+
+export interface TransactionDetails {
+  uuid: string,
+  note: string,
+  time: string,
+  type: string,
+  amount: number,
+  category: string
+}
 
 export interface CreateTransaction {
   amount: number,
