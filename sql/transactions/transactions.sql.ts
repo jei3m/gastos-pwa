d16b28dcbf4b13241ec07d749d4edaeb0b9b6cd3
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-16 19:07:11 +0800

    feat: (#6) store functions for get and get specific transactions

--- sql/transactions/transactions.sql.ts
+++ sql/transactions/transactions.sql.ts
@@ -16,6 +16,38 @@ export const createTransaction = () => {
             SELECT @response AS response;`;
 };
 
+export const getTransactions = () => {
+    return `SELECT 
+                date,
+                total,
+                details
+            FROM
+                v_transactions
+            WHERE
+                userID = :userID
+                AND accountID = :accountID
+                AND (:dateStart IS NULL OR :dateStart <= date)
+                AND (:dateEnd IS NULL OR :dateEnd >= date);`;
+};
+
+export const getTransactionByID = () => {
+    return `SELECT
+                uuid,
+                note,
+                amount,
+                type,
+                time,
+                date,
+                refCategoriesID,
+                refUserID
+            FROM
+                v_transaction_details
+            WHERE
+                uuid = :uuid
+                AND refUserID = :userID
+            LIMIT 1;`;
+};
+
 export const updateTransaction = () => {
     return `CALL manage_transactions
             (
