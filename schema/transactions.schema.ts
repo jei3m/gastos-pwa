db1aa43ef60873ee23319f4f49e86fa1711e419f
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-21 21:28:35 +0800

    ref: add minimum value for amount, change schema name

--- schema/transactions.schema.ts
+++ schema/transactions.schema.ts
@@ -1,12 +1,16 @@
 import { z } from "zod"
 
-export const createTransactionSchema = z.object({
+export const transactionSchema = z.object({
   note: z.string().max(20, {
     message: "Note has a maximum of 20 characters only",
   }),
-  amount: z.string().regex(/^\d{1,10}(\.\d{1,2})?$/, {
-    message: "Amount must be a valid decimal with up to 2 decimal places"
-  }),
+  amount: z.string()
+    .regex(/^\d{1,10}(\.\d{1,2})?$/, {
+      message: "Amount must be a valid decimal with up to 2 decimal places"
+    })
+    .refine((val) => parseFloat(val) > 0, {
+      message: "Amount must be greater than 0"
+    }),
   type: z.enum(['income', 'expense', ''], {
     message: "Transaction type can only be income or expense"
   }),
