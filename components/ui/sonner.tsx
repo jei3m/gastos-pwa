e95fd5ce1835328d5bb8329cee83d70ad02ca876
Justin Miguel Reyes <virozzegt@gmail.com>
2025-11-30 17:31:58 +0800

    style: updated icons

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,7 +1,13 @@
 "use client"
-
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { 
+  CircleCheckIcon, 
+  InfoIcon, 
+  Loader2Icon, 
+  OctagonXIcon, 
+  TriangleAlertIcon 
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
@@ -9,6 +15,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      icons={{
+        success: <CircleCheckIcon className="size-4" />,
+        info: <InfoIcon className="size-4" />,
+        warning: <TriangleAlertIcon className="size-4" />,
+        error: <OctagonXIcon className="size-4" />,
+        loading: <Loader2Icon className="size-4 animate-spin" />,
+      }}
       toastOptions={{
         style: {
           background: "var(--normal-bg)",
