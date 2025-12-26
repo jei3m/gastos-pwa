afc0dfedde638f72d938bfd223dc42dea27da14a
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-26 15:13:28 +0800

    chore: add image domain

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import withPWA from '@ducanh2912/next-pwa';
 const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
-  turbopack: {}
+  turbopack: {},
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 const isDev = process.env.NODE_ENV === 'development';
