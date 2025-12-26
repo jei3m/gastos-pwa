8d58da3866ea80f43762e508e9849261e2936c9c
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-26 11:48:46 +0800

    ref: modularize menuItems

--- lib/data.ts
+++ lib/data.ts
@@ -1,5 +1,26 @@
+import { DockItem } from "@/types/dock.types";
+import { ArrowLeftRight, Landmark, Tags } from "lucide-react";
+
 export const tabItems = [{ value: "weekly" }, { value: "monthly" }, { value: "yearly" }];
 
 export const categoryTypes = ["Expense", "Income"];
 
 export const transactionTypes = ["Expense", "Income"];
+
+export const menuItems: DockItem[] = [
+  { 
+    label: 'transactions', 
+    icon: ArrowLeftRight, 
+    route:'/pages/transactions' 
+  },
+  {
+    label: 'accounts', 
+    icon: Landmark, 
+    route:'/pages/accounts' 
+  },
+  { 
+    label: 'categories', 
+    icon: Tags, 
+    route:'/pages/categories' 
+  },
+];
