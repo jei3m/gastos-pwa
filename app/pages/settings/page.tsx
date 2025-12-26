d9b8469de7974160ca9a96ddbf5a6832e065bc8f
Justin Miguel Reyes <virozzegt@gmail.com>
2025-12-26 17:54:15 +0800

    feat: accounts, and categories section

--- app/pages/settings/page.tsx
+++ app/pages/settings/page.tsx
@@ -1,21 +1,36 @@
 "use client";
-import { TypographyH5 } from '@/components/custom/typography';
+import { TypographyH4, TypographyH5 } from '@/components/custom/typography';
 import { Separator } from '@/components/ui/separator';
 import { createAuthClient } from 'better-auth/react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { CalendarDays, User, Clock, LogOut } from 'lucide-react';
+import { CalendarDays, User, Clock, LogOut, PlusIcon } from 'lucide-react';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useAccount } from '@/context/account-context';
+import PulseLoader from '@/components/custom/pulse-loader';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Account } from '@/types/accounts.types';
+import Link from 'next/link';
+import { categoryQueryOptions } from '@/lib/tq-options/categories.tq.options';
+import { useQuery } from '@tanstack/react-query';
+import { useMemo, useState } from 'react';
+import { TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { categoryTypes } from '@/lib/data';
+import { Tabs } from '@radix-ui/react-tabs';
+import { Category } from '@/types/categories.types';
+import CategoryCard from '@/components/categories/category-card';
 
 const authClient = createAuthClient();
 
 export default function Settings() {
+  const [activeTab, setActiveTab] = useState<string>("expense");
   const { data: session } = authClient.useSession();
   const isMobile = useIsMobile();
   const router = useRouter();
+  const { accounts, isAccountsLoading, selectedAccountID } = useAccount();
 
   const handleLogout = async() => {
     authClient.signOut({
@@ -40,27 +55,28 @@ export default function Settings() {
     }
   };
 
+  // Fetch categories
+  const { data: categoriesData, isPending: isCategoriesLoading } = useQuery(
+    categoryQueryOptions(
+      activeTab!,
+      selectedAccountID!
+    )
+  );
+  const categories = useMemo(() => {
+    return categoriesData?.[0]?.details;
+  }, [categoriesData]);
+
   return (
-    <main className='space-y-8 px-3 mx-auto'>
-      {/* Account Section */}
-      <section className={cn('space-y-4', isMobile && 'mt-2')}>
-
-        {!isMobile && (
-          <div>
-            <TypographyH5 className='font-semibold text-lg mt-4'>
-              Account Overview
-            </TypographyH5>
-            <Separator className='mt-2 bg-muted-foreground' />
-          </div>          
-        )}
-
+    <main className={cn('space-y-6 px-3 mx-auto', isMobile ? 'pb-20' : 'pb-4')}>
+      {/* User Section */}
+      <section className={cn('space-y-4')}>
         {/* Profile Card */}
-        <Card className="border-2">
+        <Card className={cn('border-2 mt-4')}>
           <CardHeader className="pb-4">
             <CardTitle className="text-lg flex justify-between">
               <div className='flex gap-2 items-center'>
                 <User className="h-5 w-5" />
-                Account Information
+                User Information
               </div>
               {!isMobile && (
                 <Button 
@@ -137,6 +153,109 @@ export default function Settings() {
             </div>
           </CardFooter>
         </Card>
+      </section>
+
+      {/* Accounts Section */}
+      <section className={cn('space-y-4')}>
+        <TypographyH5 className='font-semibold text-lg'>
+          Accounts
+        </TypographyH5>          
+        <Separator className='-mt-2 bg-muted-foreground' />
+        {isAccountsLoading || !accounts ? (
+          <PulseLoader/>
+        ):(
+          <>
+            <div className="grid md:grid-cols-2 gap-2">
+              {accounts.map((account: Account) => (
+                <Link
+                  href={`/pages/accounts/${account.id}`}
+                  key={account.id}
+                >
+                  <Card className='border-2 h-full'>
+                    <CardHeader>
+                      <div className='flex flex-rows items-center justify-between'>
+                        <div className='text-xl font-bold'>
+                          {
+                            isAccountsLoading 
+                              ? <Skeleton className='h-4 w-[140px] bg-gray-300' /> 
+                              : account?.name
+                          }
+                        </div>
+                        <div className='text-md text-gray-600 font-normal'>
+                          {
+                            isAccountsLoading 
+                              ? <Skeleton className='h-4 w-[140px] bg-gray-300' />
+                              : account?.type
+                          }
+                        </div>
+                      </div>
+                    </CardHeader>
+                    <Separator className='-mt-2'/>
+                    <CardContent className='space-y-2 break-all'>
+                      {account.description}
+                    </CardContent>
+                  </Card>              
+                </Link>
+              ))}
+            </div>
+            <Link className='w-full' href={'/pages/accounts/add'}>
+              <Button 
+                className='w-full'
+              >
+                <PlusIcon size={40} className='-mr-1'/> Add New Account
+              </Button>              
+            </Link>   
+          </>
+        )} 
+      </section>
+
+      {/* Categories Section */}
+      <section className={cn('space-y-4')}>
+        <Tabs value={activeTab} onValueChange={setActiveTab}>
+          <div className='flex justify-between items-center'>
+            <TypographyH4 className='font-semibold text-lg'>
+              Categories
+            </TypographyH4> 
+            <TabsList defaultValue="expense" className='border-black border-2 p-1'>
+                {categoryTypes.map((type, index) => (
+                  <TabsTrigger
+                    value={type.toLowerCase()}
+                    key={index}
+                    className={`${
+                      type === 'Expense'
+                        ? 'data-[state=active]:bg-red-400'
+                        : 'data-[state=active]:bg-green-300'
+                    }`}
+                  >
+                    {type}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+          </div>
+        </Tabs>
+        <Separator className='-mt-2 bg-muted-foreground' />
+        {isCategoriesLoading || !categories ? (
+          <PulseLoader/>
+        ):(
+          <>
+            <div className="grid md:grid-cols-2 gap-2">
+              {categories.map((category: Category) => (
+                <CategoryCard 
+                  key={category.id}
+                  category={category}
+                  hideAmount={true}
+                />
+              ))}
+            </div>
+            <Link className='w-full' href={'/pages/categories/add'}>
+              <Button 
+                className='w-full'
+              >
+                <PlusIcon size={40} className='-mr-1'/> Add New Category
+              </Button>              
+            </Link>
+          </>
+        )} 
       </section>
     </main>
   );
