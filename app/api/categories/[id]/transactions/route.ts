7b30dcdec5ffa437d6e0707719c85eefddbec087
Justin Miguel Reyes <virozzegt@gmail.com>
2026-02-01 14:55:52 +0800

    feat: API endpoint to get transactions by category with pagination

---
+++ app/api/categories/[id]/transactions/route.ts
@@ -0,0 +1,76 @@
+import { db } from '@/utils/db';
+import { success, fail } from '@/utils/helpers';
+import { fetchUserID } from '@/lib/auth/auth-session';
+import { connection } from '@/utils/db';
+import {
+  getTransactionsByCategory,
+  getTransactionsByCategoryCount,
+} from '@/lib/sql/transactions/transactions.sql';
+import { RowDataPacket } from 'mysql2';
+
+// Fetch Transactions By Category with Pagination
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    const url = new URL(request.url);
+    const accountID = url.searchParams.get('accountID');
+    const page = parseInt(
+      url.searchParams.get('page') || '1',
+      10
+    );
+    const dateStart =
+      url.searchParams.get('dateStart') || null;
+    const dateEnd = url.searchParams.get('dateEnd') || null;
+    const limit = 10;
+    const offset = (page - 1) * limit;
+    const userID = await fetchUserID();
+
+    if (!accountID) {
+      throw Error('There is no selected account');
+    }
+
+    const [transactionCount] = await db.query<
+      RowDataPacket[]
+    >(getTransactionsByCategoryCount(), {
+      userID,
+      accountID,
+      categoryID: id,
+      dateStart,
+      dateEnd,
+    });
+
+    const [rows] = await db.query<RowDataPacket[]>(
+      getTransactionsByCategory(),
+      {
+        userID,
+        accountID,
+        categoryID: id,
+        limit,
+        offset,
+        dateStart,
+        dateEnd,
+      }
+    );
+
+    const hasMore =
+      offset + limit < transactionCount[0].count;
+
+    return success({
+      hasMore: hasMore,
+      currentPage: page,
+      data: rows,
+    });
+  } catch (error) {
+    return fail(
+      500,
+      error instanceof Error
+        ? error.message
+        : 'Failed to Fetch Transactions'
+    );
+  } finally {
+    connection.release();
+  }
+}
