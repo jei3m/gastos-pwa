a2c1ff9e86da3b02b936bbe2291b64fda8367a76
Justin Miguel Reyes <virozzegt@gmail.com>
2026-03-03 08:32:29 +0800

    feat: dynamic toast location

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Navbar from '@/components/custom/navbar';
 import { Dock } from '@/components/custom/dock';
 import { Toaster } from '@/components/ui/sonner';
@@ -5,10 +6,12 @@ import { Suspense } from 'react';
 import Loader from '@/components/custom/loader';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/ui/app-sidebar';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export default function TransactionsLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const isMobile = useIsMobile();
   return (
     <Suspense fallback={<Loader />}>
       <SidebarProvider>
@@ -18,7 +21,12 @@ export default function TransactionsLayout({
           <div className="max-w-[800px] m-auto">
             {children}
           </div>
-          <Toaster position="top-center" expand={false} />
+          <Toaster
+            position={
+              isMobile ? 'top-center' : 'bottom-right'
+            }
+            expand={false}
+          />
           <Dock showLabels={true} />
         </div>
       </SidebarProvider>
