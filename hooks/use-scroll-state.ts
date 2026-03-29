dd95e3f552d731ebf910fd21c653a2f1609d0120
Justin Miguel Reyes <virozzegt@gmail.com>
2026-03-29 17:06:52 +0800

    fix: non-mobile support

--- hooks/use-scroll-state.ts
+++ hooks/use-scroll-state.ts
@@ -6,24 +6,39 @@ import {
 
 export function useScrollState(
   ref: RefObject<HTMLElement | null>,
-  threshold: number = 20
+  threshold: number = 20,
+  isMobile: boolean = true
 ) {
   const [isScrolled, setIsScrolled] = useState(false);
 
   useLayoutEffect(() => {
-    const sr = ref.current;
-    if (!sr) return;
+    if (isMobile) {
+      const sr = ref.current;
+      if (!sr) return;
 
-    setIsScrolled(sr.scrollTop > threshold);
-
-    const onScroll = () => {
       setIsScrolled(sr.scrollTop > threshold);
-    };
-    sr.addEventListener('scroll', onScroll, {
-      passive: true,
-    });
-    return () => sr.removeEventListener('scroll', onScroll);
-  }, [ref, threshold]);
+
+      const onScroll = () => {
+        setIsScrolled(sr.scrollTop > threshold);
+      };
+      sr.addEventListener('scroll', onScroll, {
+        passive: true,
+      });
+      return () =>
+        sr.removeEventListener('scroll', onScroll);
+    } else {
+      setIsScrolled(window.scrollY > threshold);
+
+      const onScroll = () => {
+        setIsScrolled(window.scrollY > threshold);
+      };
+      window.addEventListener('scroll', onScroll, {
+        passive: true,
+      });
+      return () =>
+        window.removeEventListener('scroll', onScroll);
+    }
+  }, [ref, threshold, isMobile]);
 
   return isScrolled;
 }
