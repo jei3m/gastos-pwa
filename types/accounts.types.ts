eaf72b03392987c0fc481a47b76b11149acfba36
Justin Miguel Reyes <virozzegt@gmail.com>
2026-05-01 20:09:34 +0800

    feat: support for isDropdown

--- types/accounts.types.ts
+++ types/accounts.types.ts
@@ -4,16 +4,19 @@ export interface Account {
   type: string;
   description: string;
   totalBalance: string;
+  isDropdown: number;
 }
 
 export interface CreateAccount {
   name: string;
   type: string;
   description: string;
+  isDropdown: number;
 }
 
 export interface EditAccount {
   name: string;
   type: string;
   description: string;
+  isDropdown: number;
 }
