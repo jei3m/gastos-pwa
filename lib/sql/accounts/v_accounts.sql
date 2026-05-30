6c789120249423b8a3d6fc6912d80d066f552157
Justin Miguel Reyes <virozzegt@gmail.com>
2026-05-30 13:37:36 +0800

    feat: join account_members

--- lib/sql/accounts/v_accounts.sql
+++ lib/sql/accounts/v_accounts.sql
@@ -2,13 +2,17 @@ CREATE OR REPLACE VIEW `v_accounts` AS
 
 /* Select Query */
 SELECT 
-    id,
-    ref_user_id,
-    name,
-    type,
-    description,
-    total_balance AS totalBalance,
-    is_dropdown AS isDropdown
-FROM 
-    accounts;
+    a.id,
+    a.ref_user_id,
+    a.name,
+    a.type,
+    a.description,
+    a.total_balance AS totalBalance,
+    a.is_dropdown AS isDropdown,
+    JSON_ARRAYAGG(
+        am.user_id
+    ) AS ref_user_ids
+FROM accounts a
+LEFT JOIN account_members am ON a.id = am.account_id
+GROUP BY a.id, a.ref_user_id, a.name, a.type, a.description, a.total_balance, a.is_dropdown;
 /* END Select Query */
