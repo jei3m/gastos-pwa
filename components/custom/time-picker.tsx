618d54cbe9dd24265f8536494c1765065192d88d
Justin Miguel Reyes <virozzegt@gmail.com>
2026-05-30 14:39:11 +0800

    ref: code formatting

--- components/custom/time-picker.tsx
+++ components/custom/time-picker.tsx
@@ -109,14 +109,18 @@ export function TimePicker({
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild disabled={disabled || readOnly}>
+      <PopoverTrigger
+        asChild
+        disabled={disabled || readOnly}
+      >
         <Button
           variant="outline"
           type="button"
           className={cn(
             'bg-white border-2 w-full justify-between text-left font-normal h-9 md:h-10',
             !value && 'text-muted-foreground',
-            readOnly && 'bg-gray-50 border-dashed border-gray-300 disabled:opacity-100 disabled:cursor-default'
+            readOnly &&
+              'bg-gray-50 border-dashed border-gray-300 disabled:opacity-100 disabled:cursor-default'
           )}
         >
           <span className="text-[16px] text-black">
